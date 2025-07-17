a909f5eff9ebdaebbcb6d862accbc8b8d9f55470
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-07-17 16:16:34 -0700

    feat: basic quote command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
 use poise::serenity_prelude as serenity;
 use dotenvy::dotenv;
 use std::env;
 
-struct Data{}
+#[derive(Debug, Clone)]
+struct Quote {
+  quoted_by: serenity::UserId,
+  quoted_user: serenity::UserId,
+  quoted_text: String,
+}
+
+#[derive(Default)]
+struct Data{
+  quotes: Arc<Mutex<Vec<Quote>>>,
+}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(slash_command)]
+async fn quote(
+  ctx: Context<'_>,
+  #[description = "Who are you quoting?"] user: serenity::User,
+  #[description = "What did they say?"] text: String,
+) -> Result<(), Error> {
+  let quote = Quote{
+    quoted_by: ctx.author().id,
+    quoted_user: user.id,
+    quoted_text: text.clone(),
+  };
+
+  {
+    let mut quotes = ctx.data().quotes.lock().await;
+    quotes.push(quote.clone());
+  }
+
+  ctx.say(format!(
+    "Logged quote. Quoted by <@{}>\nQuoted user<@{}>\n> {}",
+    quote.quoted_by,
+    quote.quoted_user,
+    quote.quoted_text
+  ))
+  .await?;
+  
+  Ok(())
+}
 
 #[poise::command(slash_command)]
 async fn ping(
@@ -21,19 +62,19 @@ async fn main() {
 
   let intents = serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::GUILD_MESSAGES | serenity::GatewayIntents::MESSAGE_CONTENT;
 
-  
-
   let framework = poise::Framework::builder()
     .options(poise::FrameworkOptions::<Data, Error> {
-      commands: vec![ping()],
+      commands: vec![ping(), quote()],
       ..Default::default()
     })
     .setup(|ctx, _ready, framework| {
       Box::pin(async move {
         let guild_id = serenity::GuildId::new(1073078539051614259);
-        poise::builtins::register_in_guild(ctx, &framework.options().commands, guild_id).await?; //temp for guild
+        poise::builtins::register_in_guild(ctx, &framework.options().commands, guild_id).await?; // using guild id temp for dev
         println!("{} is connected. Hello, world!", ctx.cache.current_user().name);
-        Ok(Data {})
+        Ok(Data {
+          quotes: Arc::new(Mutex::new(Vec::new())),
+        })
       })
     })
     .build();
